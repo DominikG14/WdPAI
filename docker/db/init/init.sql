fab2ebbef14ebf2ea5dbf969a68cfb1fd157507c
DominikG14 <dgasiorek14@gmail.com>
2026-06-03 11:54:21 +0200

    added sample exercises

--- docker/db/init/init.sql
+++ docker/db/init/init.sql
@@ -18,7 +18,11 @@ CREATE TABLE exercises (
     field_id INT REFERENCES fields(id) ON DELETE CASCADE,
     image_url TEXT,
     type VARCHAR(4) NOT NULL CHECK (type IN ('ABCD', 'PF')),
-    right_answer TEXT NOT NULL
+    right_answer TEXT NOT NULL,
+    CONSTRAINT check_right_answer_format CHECK (
+        (type = 'ABCD' AND right_answer ~ '^[A-D]$') OR
+        (type = 'PF' AND right_answer ~ '^[PF]{2}$')
+    )
 );
 
 CREATE TABLE roles (
@@ -34,7 +38,6 @@ CREATE TABLE user_roles (
 
 INSERT INTO roles (name) VALUES 
 ('ADMIN'),
-('USER');
 
 INSERT INTO fields (number, name) VALUES
 ('I', 'Liczby rzeczywiste'),
@@ -51,3 +54,62 @@ INSERT INTO fields (number, name) VALUES
 ('XII', 'Prawdopodobieństwo'),
 ('XIII', 'Statystyka'),
 ('XIV', 'Optymalizacja');
+
+INSERT INTO exercises (field_id, image_url, type, right_answer) VALUES
+(1, 'public/images/1/1.png', 'ABCD', 'C'),
+(1, 'public/images/1/2.png', 'ABCD', 'C'),
+(1, 'public/images/1/3.png', 'ABCD', 'B'),
+(1, 'public/images/1/4.png', 'ABCD', 'B'),
+(1, 'public/images/1/5.png', 'ABCD', 'B'),
+(1, 'public/images/1/6.png', 'ABCD', 'C'),
+(1, 'public/images/1/7.png', 'ABCD', 'B'),
+(1, 'public/images/1/8.png', 'PF', 'PP'),
+(2, 'public/images/2/1.png', 'ABCD', 'A'),
+(2, 'public/images/2/2.png', 'ABCD', 'A'),
+(2, 'public/images/2/3.png', 'ABCD', 'A'),
+(3, 'public/images/3/1.png', 'ABCD', 'B'),
+(3, 'public/images/3/2.png', 'ABCD', 'C'),
+(3, 'public/images/3/3.png', 'ABCD', 'D'),
+(4, 'public/images/4/1.png', 'ABCD', 'D'),
+(5, 'public/images/5/1.png', 'PF', 'PF'),
+(5, 'public/images/5/2.png', 'ABCD', 'C'),
+(5, 'public/images/5/3.png', 'ABCD', 'A'),
+(5, 'public/images/5/4.png', 'ABCD', 'D'),
+(5, 'public/images/5/5.png', 'PF', 'FF'),
+(5, 'public/images/5/6.png', 'ABCD', 'A'),
+(5, 'public/images/5/7.png', 'ABCD', 'D'),
+(6, 'public/images/6/1.png', 'ABCD', 'C'),
+(6, 'public/images/6/2.png', 'PF', 'PP'),
+(6, 'public/images/6/3.png', 'ABCD', 'C'),
+(6, 'public/images/6/4.png', 'ABCD', 'B'),
+(7, 'public/images/7/1.png', 'ABCD', 'B'),
+(7, 'public/images/7/2.png', 'ABCD', 'A'),
+(7, 'public/images/7/3.png', 'ABCD', 'C'),
+(7, 'public/images/7/4.png', 'ABCD', 'D'),
+(8, 'public/images/8/1.png', 'ABCD', 'C'),
+(8, 'public/images/8/2.png', 'ABCD', 'B'),
+(8, 'public/images/8/3.png', 'ABCD', 'C'),
+(8, 'public/images/8/4.png', 'ABCD', 'B'),
+(9, 'public/images/9/1.png', 'ABCD', 'B'),
+(9, 'public/images/9/2.png', 'ABCD', 'D'),
+(9, 'public/images/9/3.png', 'ABCD', 'B'),
+(9, 'public/images/9/4.png', 'ABCD', 'D'),
+(9, 'public/images/9/5.png', 'PF', 'PF'),
+(10, 'public/images/10/1.png', 'ABCD', 'D'),
+(10, 'public/images/10/2.png', 'PF', 'PP'),
+(11, 'public/images/11/1.png', 'ABCD', 'A'),
+(11, 'public/images/11/2.png', 'ABCD', 'A'),
+(13, 'public/images/13/1.png', 'ABCD', 'A'),
+(13, 'public/images/13/2.png', 'ABCD', 'C'),
+(13, 'public/images/13/3.png', 'PF', 'PP'),
+(13, 'public/images/13/4.png', 'ABCD', 'C'),
+(14, 'public/images/14/1.png', 'ABCD', 'D'),
+(14, 'public/images/14/2.png', 'ABCD', 'A');
+
+INSERT INTO users (username, email, password) VALUES 
+('admin', 'admin@example.com', 'admin');
+
+INSERT INTO user_roles (user_id, role_id) VALUES (
+    (SELECT id FROM users WHERE username = 'admin'),
+    (SELECT id FROM roles WHERE name = 'ADMIN')
+);
